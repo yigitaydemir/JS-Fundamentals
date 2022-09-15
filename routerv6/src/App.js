60893aacaa94d46ef972e541225143cf139a0255
Yusuf Yigit Aydemir <yusufyigitaydemir@gmail.com>
2022-09-15 22:18:30 +0300

    devam

--- routerv6/src/App.js
+++ routerv6/src/App.js
@@ -14,11 +14,20 @@ function App() {
         <h1>Hello this is my tutorial to React Router V6</h1>
         <Nav></Nav>
         <Routes>
-          <Route path="/" element={<Home/>}></Route>
-          <Route path="/about" element={<About/>}></Route>
-          <Route path="/books" element={<BooksList/>}></Route>
-          <Route path="/books/:id" element={<Book/>}></Route>
-          <Route path="*" element={<NotFound/>}></Route>
+          <Route path="/" element={<Home />}></Route>
+          <Route path="/about" element={<About />}></Route>
+          
+          {/* <Route path="/books" element={<BooksList/>}></Route>
+          <Route path="/books/:id" element={<Book/>}></Route> */}
+
+          {/* Nested Routes */}
+
+          <Route path="/books">
+            <Route index element={<BooksList />}></Route>
+            <Route path=":id" element={<Book />}></Route>
+          </Route>
+
+          <Route path="*" element={<NotFound />}></Route>
         </Routes>
       </div>
   );
